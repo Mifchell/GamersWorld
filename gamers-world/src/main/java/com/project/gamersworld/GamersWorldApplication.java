f65f22114cb736e80411a30e892c99784ad80322
EmmaRouger <58708911+EmmaRouger@users.noreply.github.com>
2023-02-19 17:11:57 -0600

    add spring to main
    add spring project

---
+++ gamers-world/src/main/java/com/project/gamersworld/GamersWorldApplication.java
@@ -0,0 +1,13 @@
+package com.project.gamersworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamersWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamersWorldApplication.class, args);
+	}
+
+}
---
+++ gamers-world/src/test/java/com/project/gamersworld/GamersWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.gamersworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamersWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
