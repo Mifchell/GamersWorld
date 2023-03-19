d4c6f20766a9b618b2ad86d50b792c9262415f94
EmmaRouger <58708911+EmmaRouger@users.noreply.github.com>
2023-03-19 14:52:54 -0500

    create new classes

---
+++ gamers-world/src/main/java/com/project/gamersworld/PlayLevel.java
@@ -0,0 +1,19 @@
+package com.project.gamersworld;
+
+public enum PlayLevel {
+    DEBUTANT("Debutant"),
+    EXPERT("Expert"),
+    CASUAL("Casual"),
+    WANTING_TO_LEARN("Wanting to Learn");
+
+    private String enumValue;
+
+    private PlayLevel(String enumValue) {
+        this.enumValue = enumValue;
+    }
+
+    public String getEnumValue() {
+        return enumValue;
+    }
+
+}
