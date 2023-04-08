642acfa80b377d15b11cfe5be2b0914780ce2eb5
EmmaRouger <58708911+EmmaRouger@users.noreply.github.com>
2023-04-07 21:44:27 -0500

    changes

--- gamers-world/src/main/java/com/project/gamersworld/handlers/EventHandler.java
+++ gamers-world/src/main/java/com/project/gamersworld/handlers/EventHandler.java
@@ -1,26 +1,52 @@
 package com.project.gamersworld.handlers;
 
-import com.project.gamersworld.models.Group;
-import com.project.gamersworld.models.User;
+import com.project.gamersworld.models.*;
+import com.project.gamersworld.repo.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class EventHandler {
-    private List<Group> eventRepository;
+    @Autowired
+    private EventRepo eventRepo;
 
     public EventHandler() {
 
     }
 
-    public List<Group> getGroupRepository() {
-        return this.eventRepository;
+    public EventRepo getEventRepository() {
+        return this.eventRepo;
     }
 
     /*
      * Does a event search based on user preference
      */
-    public void eventSearch(User user) {
+    public List<Event> eventSearch(User user) {
+        // all events
+        List<Event> events = eventRepo.findAll();
+        // create empty events list
+        List<Event> returnEvents = new ArrayList<Event>();
 
+        // check if user has a preference
+        if (!user.getProfile().getGames().isEmpty()) {
+            // retrieve events with matching user preferences
+            for (int i = 0; i < events.size(); i++) {
+                for (int j = 0; j < user.getProfile().getGames().size(); j++) {
+                    if (events.get(i).getGame().equals(user.getProfile().getGames().get(i))) {
+                        // store event in return list
+                        returnEvents.add(events.get(i));
+                    }
+                }
+            }
+        } else {
+            // retrieve all events sorted chronologically
+            returnEvents = events;
+
+        }
+
+        return events;
     }
 
     /*
