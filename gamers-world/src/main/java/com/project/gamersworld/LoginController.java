cf2c9c274bab29da6ba7d11ad969cac86f34e312
ttabithar <125708289+ttabithar@users.noreply.github.com>
2023-04-13 00:11:48 -0500

    wip

--- gamers-world/src/main/java/com/project/gamersworld/LoginController.java
+++ gamers-world/src/main/java/com/project/gamersworld/LoginController.java
@@ -1,29 +1,59 @@
 package com.project.gamersworld;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-    @GetMapping(value = "/login")
-    public String showLoginForm() {
-        return "login";
-    }   
+    @Autowired
+    private UserRepo userRepo;
 
-    @PostMapping("/login")
-    public String login(@RequestParam String email, @RequestParam String password) {
-        // Authenticate user
-
-        return "redirect:/index";
+    @GetMapping("")
+    public String viewHomePage(){
+        return "index";
     }
 
-    //necessary?
-    @GetMapping("/")
-    public String showHomePage() {
+    // log in
+    @GetMapping("/login")
+    public String showLoginForm(Model model) {
+        List<User> users = userRepo.findAll();
+        model.addAttribute("user", users);
+        return "login";
+    }
+// error is that user is null
+    @PostMapping("/login")
+    public String login(@ModelAttribute User user) {
+      // authenticate user
+      // traverse the list!
+      if ("email".equals(users.get(i)) && "password".equals(user.getProfile().getPassword())) {
         return "redirect:/index";
+      } else {
+        return "redirect:/login?error";
+      }
+    }
+
+
+    // sign up
+    @GetMapping("/signup")
+    public String showSignUp(Model model)
+    {
+        model.addAttribute("user", new User());
+        return "signup";
+    }
+
+    @PostMapping("/process_signup")
+    public String processSignUp(User user)
+    {
+        // save User
+        userRepo.save(user);
+        return "editprofile";
     }
 
 }
