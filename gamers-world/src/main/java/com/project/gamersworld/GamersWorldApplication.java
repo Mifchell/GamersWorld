8af4ea99394934244803221a8c692b7e4888ec96
idfkst <jmlecik@ilstu.edu>
2023-04-14 13:43:59 -0500

    more

--- gamers-world/src/main/java/com/project/gamersworld/GamersWorldApplication.java
+++ gamers-world/src/main/java/com/project/gamersworld/GamersWorldApplication.java
@@ -9,15 +9,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
-public class GamersWorldApplication implements CommandLineRunner {
 
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class GamersWorldApplication implements CommandLineRunner {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GamersWorldApplication.class, args);
 	}
 
 	@Override
-	public void run(String... args) {
+	public void run(String... args){
 
 	}
 
