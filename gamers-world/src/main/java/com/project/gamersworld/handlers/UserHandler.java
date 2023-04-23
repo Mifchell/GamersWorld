18bc19f32e126808e38f0e8ca572cade2bf0e6ad
ykcilb <nctr834@gmail.com>
2023-04-23 03:23:45 -0500

    Update UserHandler.java
    fixed merge conflict

--- gamers-world/src/main/java/com/project/gamersworld/handlers/UserHandler.java
+++ gamers-world/src/main/java/com/project/gamersworld/handlers/UserHandler.java
@@ -2,6 +2,7 @@ package com.project.gamersworld.handlers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -17,32 +18,29 @@ public class UserHandler {
     @Autowired
     private UserRepo userRepo;
 
-    public boolean login(String email, String password) {
-        boolean check = false;
-
+    public User login(String email, String password) {
         // if user exists, check if password matches saved password
         if (userRepo.findByProfileEmailAddress(email) != null) {
             User user = new User(userRepo.findByProfileEmailAddress(email));
-            check = user.getProfile().getPassword().equals(password);
+            if (user.getProfile().getPassword().equals(password)) {
+                return user;
+            }
         }
 
-        return check;
+        return null;
     }
 
-    public boolean signUp(String email, String password) {
-
-        boolean check = false;
-
+    public User signUp(String email, String password) {
         // check if there is already an existing email
         if (userRepo.findByProfileEmailAddress(email) != null) {
-            return check;
+            return null;
         } else {
             // create new profile and user
             Profile profile = new Profile("", password, email, "", "");
             User user = new User(profile);
             userRepo.save(user);
 
-            return true;
+            return user;
         }
     }
 
@@ -90,4 +88,21 @@ public class UserHandler {
         return returnList;
     }
 
+    public void createProfile(User user, String username, String description, String preferredTime, String game) {
+        user.getProfile().setUsername(username);
+        user.getProfile().setDescription(description);
+        user.getProfile().setTime(preferredTime);
+
+        // set one game for now
+        List<Game> games = new ArrayList<Game>();
+        games.add(Game.valueOf(game));
+        user.getProfile().setGames(games);
+
+        // save all
+        userRepo.save(user);
+    }
+
+    public UserRepo getUserRepo() {
+        return userRepo;
+    }
 }
