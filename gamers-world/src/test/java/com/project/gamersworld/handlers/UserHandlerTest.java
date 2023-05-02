9a92ecf38003718be65a45bdbf430a913c4308de
ttabithar <125708289+ttabithar@users.noreply.github.com>
2023-05-02 00:34:23 -0500

    good

--- gamers-world/src/test/java/com/project/gamersworld/handlers/UserHandlerTest.java
+++ gamers-world/src/test/java/com/project/gamersworld/handlers/UserHandlerTest.java
@@ -2,6 +2,7 @@ package com.project.gamersworld.handlers;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
@@ -10,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.aspectj.lang.annotation.Before;
+import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -33,6 +35,7 @@ public class UserHandlerTest {
 
     private User user1;
     private User user2;
+    private boolean usernameGood;
 
     @BeforeEach
     void setUp()
@@ -42,9 +45,11 @@ public class UserHandlerTest {
         userHandler = new UserHandler(mockUserRepository);
 
         // set up users
-        Profile profile = new Profile("user1", "1234", "test1@test.com", "", "");
+        Profile profile = new Profile("", "1234", "test1@test.com", "", "");
         user1 = new User(profile);
         user2 = new User();
+
+        usernameGood = false;
 
     }
 
@@ -88,9 +93,9 @@ public class UserHandlerTest {
     @Test
     void testSignUpHappyPath() {
         // should return user
-        when(mockUserRepository.findByProfileEmailAddress("test1@test.com")).thenReturn(user1);
+        when(mockUserRepository.findByProfileEmailAddress("test1@test.com")).thenReturn(null);
 
-        user2 = userHandler.login("test1@test.com", "1234");
+        user2 = userHandler.signUp("test1@test.com", "1234");
 
         assertEquals(user1, user2);
     }
@@ -100,9 +105,36 @@ public class UserHandlerTest {
         // should return user
         when(mockUserRepository.findByProfileEmailAddress("test1@test.com")).thenReturn(user1);
 
-        user2 = userHandler.login("test1@test.com", "1234");
+        user2 = userHandler.signUp("test1@test.com", "1234");
 
-        assertEquals(user1, user2);
+        assertNull(user2);
     }
+
+    // test create profile
+    @Test
+    void testCreateProfileHappyPath() {
+        // should return true
+        when(mockUserRepository.findByProfileUsername("user1")).thenReturn(null);
+
+        usernameGood = userHandler.createProfile(user1, "user1", "", "",
+        new ArrayList<Game>());
+
+        // why
+        assertTrue(usernameGood);
+    }
+
+    @Test
+    void testCreateProfileExistingUsername() {
+        // should return false
+        user1.getProfile().setUsername("user1");
+        when(mockUserRepository.findByProfileUsername("user1")).thenReturn(user1);
+
+        usernameGood = userHandler.createProfile(user1, "user1", "", "",
+        new ArrayList<Game>());
+
+        assertFalse(usernameGood);
+    }
+
+
 
 }
